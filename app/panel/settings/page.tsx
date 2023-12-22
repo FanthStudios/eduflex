c6060ccc88a352ce293c0d6b68b296ef8a3f748b
Bartek Paczesny <bartek@paczesny.pl>
2023-12-22 22:56:16 +0100

    Block notifications settings tab [cant finish in time]

--- app/panel/settings/page.tsx
+++ app/panel/settings/page.tsx
@@ -21,26 +21,30 @@ function Settings({}: Props) {
         <main className="flex flex-col items-center justify-center w-full h-full gap-5 col-span-3 row-span-2">
             <Tab.Group>
                 <header className="border-b border-neutral-200 w-[106%] md:w-[109%] lg:w-[105%] 2xl:w-[104%] -mt-4">
-                    <nav className="flex overflow-x-auto py-4">
-                        <Tab.List className="flex min-w-full flex-none gap-x-6 px-4 text-sm font-semibold leading-6 text-gray-400 sm:px-6 lg:px-8">
-                            {secondaryNavigation.map((item) => (
-                                <Tab as={Fragment} key={item.name}>
-                                    {({ selected }) => (
-                                        <p
-                                            className={clsx(
-                                                selected
-                                                    ? "text-green-400"
-                                                    : "",
-                                                "cursor-pointer"
-                                            )}
-                                        >
-                                            {item.name}
-                                        </p>
-                                    )}
-                                </Tab>
-                            ))}
-                        </Tab.List>
-                    </nav>
+                    <Tab.List className="flex overflow-x-auto py-4 min-w-full flex-none gap-x-6 px-4 text-sm font-semibold leading-6 text-gray-400 sm:px-6 lg:px-8">
+                        {secondaryNavigation.map((item) => (
+                            <Tab
+                                // @ts-ignore
+                                disabled={item.name == "Powiadomienia"}
+                                as={Fragment}
+                                key={item.name}
+                            >
+                                {({ selected }) => (
+                                    <p
+                                        className={clsx(
+                                            item.name == "Powiadomienia"
+                                                ? "line-through"
+                                                : "",
+                                            selected ? "text-green-400" : "",
+                                            "cursor-pointer"
+                                        )}
+                                    >
+                                        {item.name}
+                                    </p>
+                                )}
+                            </Tab>
+                        ))}
+                    </Tab.List>
                 </header>
                 <Tab.Panels>
                     {secondaryNavigation.map((item) => (
