2d2ee9662d84e708245f23d24f0d348c57c084b9
Bartek Paczesny <bartek@paczesny.pl>
2023-12-22 19:19:17 +0100

    fix error?

--- app/api/edgestore/[...edgestore]/route.ts
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -14,11 +14,11 @@ const handler = createEdgeStoreNextHandler({
     router: edgeStoreRouter,
 });
 
+export const dynamic = "force-dynamic";
+
 export { handler as GET, handler as POST };
 
 /**
  * This type is used to create the type-safe client for the frontend.
  */
 export type EdgeStoreRouter = typeof edgeStoreRouter;
-
-export const dynamic = "force-dynamic";
