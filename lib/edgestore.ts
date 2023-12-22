8a34bc97889e693f489429a23c51b4cc6eb738ae
Bartek Paczesny <bartek@paczesny.pl>
2023-12-22 19:31:34 +0100

    does it work?????

--- lib/edgestore.ts
+++ lib/edgestore.ts
@@ -1,9 +1,8 @@
 "use client";
 
-import { type EdgeStoreRouter } from "@/app/api/edgestore/[...edgestore]/route";
+// import { type EdgeStoreRouter } from "@/app/api/edgestore/[...edgestore]/route";
 import { createEdgeStoreProvider } from "@edgestore/react";
 
-const { EdgeStoreProvider, useEdgeStore } =
-    createEdgeStoreProvider<EdgeStoreRouter>();
+const { EdgeStoreProvider, useEdgeStore } = createEdgeStoreProvider<any>();
 
 export { EdgeStoreProvider, useEdgeStore };
